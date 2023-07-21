68ed8276047968fe805366a665caf4931ba04444
danutibe07 <danielutibe07@gmail.com>
2023-07-21 16:38:13 +0100

    Made all required changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_205425) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_152814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,8 +27,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_205425) do
   create_table "categories_records", id: false, force: :cascade do |t|
     t.bigint "category_id", null: false
     t.bigint "record_id", null: false
-    t.index ["category_id", "record_id"], name: "index_categories_records_on_category_id_and_record_id"
-    t.index ["record_id", "category_id"], name: "index_categories_records_on_record_id_and_category_id"
   end
 
   create_table "records", force: :cascade do |t|
@@ -41,7 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_205425) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", limit: 50, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -49,10 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_205425) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
