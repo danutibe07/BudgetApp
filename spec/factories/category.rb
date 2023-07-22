6b569d8f24470f191a6dc386e3104d3c8ebd8160
danutibe07 <danielutibe07@gmail.com>
2023-07-22 16:27:07 +0100

    make required changes - Correct unit tests eroors

--- spec/factories/category.rb
+++
@@ -1,7 +0,0 @@
-# spec/factories/category.rb
-FactoryBot.define do
-  factory :category do
-    name { 'Sample Category' }
-    # Add any other attributes as needed
-  end
-end
--- spec/factories/user.rb
+++
@@ -1,8 +0,0 @@
-# spec/factories/user.rb
-FactoryBot.define do
-  factory :user do
-    name { 'Test User' }
-    email { 'test@example.com' }
-    password { 'password' }
-  end
-end
