a6c4acd9eb9ab31f79feced443c60eed4353d970
danutibe07 <danielutibe07@gmail.com>
2023-07-22 13:00:02 +0100

    Made required changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
-  before_action :current_user
+  include ActionView::Helpers::UrlHelper
 
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(_resource)
     categories_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 end
